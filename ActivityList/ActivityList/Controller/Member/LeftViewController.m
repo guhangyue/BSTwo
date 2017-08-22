9edd7858422a2e49e1f92000cccd85703ad345e8
HZX <1660896068@qq.com>
2017-08-22 17:38:38 +0800

    登录界面

--- ActivityList/ActivityList/Controller/Member/LeftViewController.m
+++ ActivityList/ActivityList/Controller/Member/LeftViewController.m
@@ -8,7 +8,16 @@
 
 #import "LeftViewController.h"
 
+
 @interface LeftViewController ()
+
+@property(strong,nonatomic) NSArray * arr;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+- (IBAction)loginAction:(UIButton *)sender forEvent:(UIEvent *)event;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLbl;
+@property (weak, nonatomic) IBOutlet UIImageView *avatarImgView;
+- (IBAction)settingAction:(UIButton *)sender forEvent:(UIEvent *)event;
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
 
 @end
 
@@ -17,11 +26,67 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self uiLayout];
+    [self dataInitialize];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)uiLayout
+{
+    _avatarImgView.layer.borderColor=[[UIColor lightGrayColor]CGColor];
+}
+-(void)dataInitialize
+{
+    _arr=@[@"我的活动",@"我的推广",@"积分中心",@"意见反馈",@"关于我们"];
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 2;
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section==0)
+    {
+        return _arr.count;
+    }else{
+        return 1;
+    }
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.section==0) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MemberCell" forIndexPath:indexPath];
+        
+        
+        cell.textLabel.text=_arr[indexPath.row];
+        return cell;
+    }else{
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"EmptyCell" forIndexPath:indexPath];
+        
+        return cell;
+    }
+    
+    
+}
+
+//设置cell的高度
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section==0) {
+        return 50.f;
+    }
+    else{
+        return UI_SCREEN_H-500;
+        //return UI_SCREEN_H-170-80-50*5;   等于屏幕高度减去500
+    }
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    
 }
 
 /*
@@ -34,4 +99,13 @@
 }
 */
 
+- (IBAction)loginAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    //获取要跳转的页面实例
+    UINavigationController *signNavi=[Utilities getStoryboardInstance:@"Member" byIdentity:@"SignNavi"];
+    //执行跳转
+    [self presentViewController:signNavi animated:YES completion:nil];
+
+}
+- (IBAction)settingAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
 @end
--- ActivityList/ActivityList/Controller/Sign/SignInViewController.m
+++ ActivityList/ActivityList/Controller/Sign/SignInViewController.m
@@ -17,11 +17,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self naviConfig];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (void)naviConfig {
+    //设置导航条标题文字
+    self.navigationItem.title = @"发布活动";
+    //设置导航条颜色（风格颜色）
+    self.navigationController.navigationBar.barTintColor = [UIColor darkGrayColor];
+    //设置导航条标题颜色
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+    //设置导航条是否隐藏.
+    self.navigationController.navigationBar.hidden = NO;
+    //设置导航条上按钮的风格颜色
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    //设置是否需要毛玻璃效果
+    self.navigationController.navigationBar.translucent = YES;
+    //为导航条左上角创建一个按钮
+    UIBarButtonItem *left = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop target:self action:@selector(backAction)];
+    self.navigationItem.leftBarButtonItem = left;
+}
+
+//用Model的方式返回上一页
+- (void)backAction {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    //[self.navigationController popViewControllerAnimated:YES];
 }
 
 /*
