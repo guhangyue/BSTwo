592d165430cfdacb1232f13b24a11ee2dbaf3c5a
Lubin <1589507430@qq.com>
2017-08-28 12:08:36 +0800

    shift

--- ActivityList/ActivityList/Controller/Activity/PurchaceTableViewController.m
+++ ActivityList/ActivityList/Controller/Activity/PurchaceTableViewController.m
@@ -9,7 +9,10 @@
 #import "PurchaceTableViewController.h"
 
 @interface PurchaceTableViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *contenLabel;
+@property (weak, nonatomic) IBOutlet UILabel *priceLabel;
+@property (strong,nonatomic)NSArray *arr;
 @end
 
 @implementation PurchaceTableViewController
@@ -17,6 +20,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self naviConfig];
+    [self uiLayout];
+    [self dataInitialize];
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
@@ -32,30 +37,64 @@
 - (void)naviConfig {
     //设置导航条标题文字
     self.navigationItem.title = @"活动报名支付";
+    //为导航条右上角创建一个按钮
+    UIBarButtonItem *right = [[UIBarButtonItem alloc] initWithTitle:@"支付" style:UIBarButtonItemStylePlain target:self action:@selector(payAction)];
+    self.navigationItem.rightBarButtonItem = right;
+
+}
+-(void)uiLayout{
+    _nameLabel.text= _activity.name;
+    _contenLabel.text= _activity.content;
+    _priceLabel.text=[NSString stringWithFormat:@"%@元",_activity.applyFee];
+    
+    self.tableView.tableFooterView =[UIView new];
+    //将表格视图设置为"编辑中"
+    self.tableView.editing = YES;
+}
+-(void)dataInitialize{
+    _arr=@[@"支付宝支付",@"微信支付",@"银联支付"];
+}
+-(void)payAction{
     
 }
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 #warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete implementation, return the number of rows
-    return 0;
+    return _arr.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PayCell" forIndexPath:indexPath];
+    cell.textLabel.text=_arr[indexPath.row];
     // Configure the cell...
     
     return cell;
 }
-*/
-
+//设置cell的高度
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 50.f;
+}
+//设置组的标题文字
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return @"支付方式";
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    //遍历表格视图中所有选中状态下的细胞
+    for (NSIndexPath*eachIP in tableView.indexPathsForSelectedRows) {
+        //当选中的细胞不是当前正在按的这个细胞的情况下
+        if (eachIP !=indexPath) {
+            //将细胞从选中状态改为不选中状态
+            [tableView deselectRowAtIndexPath:eachIP animated:YES];
+        }
+    }
+}
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
